1aa545ffb1d2e781cc3dad5215dd85e223f57539
mona-n67 <mona.nasrolahi@yahoo.com>
2019-07-08 23:10:50 +0430

    from class

--- main.js
+++ main.js
@@ -12,4 +12,4 @@ console.log(remover);
 output.innerHTML += "Before :  var list = ['HTML', 5, 10, true, 'HTML', 10, 1, 'HTML'] " + '<br>';
 output.innerHTML +="After   :  var list =  "  +  " [ " + remover  +  " ] "  + '<br>';
 
-//end
+//End
